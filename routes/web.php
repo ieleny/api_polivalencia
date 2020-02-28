933baab806db41c4f5a9c6c29a68cb53bad5b68b
ieleny <ifsilva@wtf.inf.br>
2020-02-28 15:42:26 -0300

    Instalando o swagger

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,27 @@ Route::get('/', function () {
 });
 
 //Inserir 
+Route::get('funcionarios_inserir',function() {
+
+});
 
 //Atualizar
+Route::get('funcionarios_atualizar',function() {
 
+});
 //Delete
+Route::get('funcionarios_delete',function() {
 
+});
 //Listar
+Route::get('funcionarios_listar',function() {
 
+});
 //Funcionarios que trabalham no Brasil
 Route::get('funcionarios_brasil',function() {
 
 });
+
+//https://medium.com/@arikardnoir/documentando-uma-api-rest-laravel-usando-swagger-2daa24b752e0
 
 
